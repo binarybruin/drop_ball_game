3f3a19f7921bd0fc109f8fda2f19d6cf60f702a7
binarybruin <michelle.bruin@gmail.com>
2015-03-31 18:01:08 -0400

    implement button scripts

---
+++ Assets/PushLeftButton.js
@@ -0,0 +1,25 @@
+﻿#pragma strict
+
+var angle : float = 60;
+
+function Start () {
+
+}
+
+function Update () {
+	if (Input.GetKeyDown(KeyCode.LeftArrow))
+		PressButton();
+		
+	if (Input.GetKeyUp(KeyCode.LeftArrow))
+		transform.localScale.y = 0.3;
+}
+
+function PressButton () {
+	
+	transform.localScale.y = 0.1;
+	
+	GameObject.Find("Panel").transform.Rotate(angle * Vector3.forward * Time.deltaTime, Space.Self);
+	yield WaitForSeconds(1);
+	GameObject.Find("Panel").transform.Rotate(angle * Vector3.back * Time.deltaTime, Space.Self);
+	
+}
